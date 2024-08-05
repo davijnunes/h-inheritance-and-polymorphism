a57c298f55cfb3ad3821331f26a4e69e317b8220
Davi Nunes <sallyn2544@gmail.com>
2024-08-05 11:57:51 -0300

    Implementamos a classe de pessoa juridica company

--- src/entities/Company.java
+++ src/entities/Company.java
@@ -1,5 +1,36 @@
 package entities;
 
-public class Company {
+public class Company extends TaxPayer {
+	
+	private Integer numberOfEmployee;
+	
+	public Company() {
+		
+	}
+	
+	public Company(String name, Double anualIncome, Integer numberOfEmployee) {
+		super(name, anualIncome);
+		this.numberOfEmployee = numberOfEmployee;
+	}
+	
+	public Integer getNumberOfEmployee() {
+		return numberOfEmployee;
+	}
+	
+	public void setNumberOfEmployee(Integer numberOfEmployee) {
+		this.numberOfEmployee = numberOfEmployee;
+	}
+	
+
+	
+	@Override
+	public double tax() {
+		
+		double taxPay = 0.0;
+		
+		taxPay = (getNumberOfEmployee() < 10) ? getAnualIncome() * 0.16 : getAnualIncome() * 0.14;
+		
+		return taxPay;
+	}
 
 }
