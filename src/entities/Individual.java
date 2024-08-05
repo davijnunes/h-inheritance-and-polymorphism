4501d014b747864b0bf13d8cf64b5585a0a5680f
Davi Nunes <sallyn2544@gmail.com>
2024-08-05 11:50:52 -0300

    Adicionamos a classe pessoa fisica e implementamos a classe generica taxPayer

---
+++ src/entities/Individual.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Individual extends TaxPayer{
+	
+	private Double heathExpenditures;
+	
+	public Individual() {
+		
+	}
+	
+	public Individual(String name, Double anualIncome,Double heathExpenditures) {
+		super(name, anualIncome);
+		this.heathExpenditures = heathExpenditures;
+	}
+	
+	
+	public Double getHeathExpenditures() {
+		return heathExpenditures;
+	}
+	
+	public void setHeathExpenditures(Double heathExpenditures) {
+		this.heathExpenditures = heathExpenditures;
+	}
+	
+	@Override
+	public double tax() {
+		
+		double taxPay = 0.0;
+		
+		// Calculation of quantity of tax a pay
+		taxPay = (getAnualIncome() > 20000.0) ? getAnualIncome() * 0.25 :  getAnualIncome() * 0.15;
+		
+		// Obtaining amounts spent on healthcare
+		taxPay -= (getHeathExpenditures() > 0.0) ? getHeathExpenditures() * 0.5 : 0;
+		
+		return Math.abs(taxPay);
+	
+		
+	}
+
+}
